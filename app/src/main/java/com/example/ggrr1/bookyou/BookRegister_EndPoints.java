64d10f671e140cc8d7c5a37761bdf0a6d8f9fc52
minyeong <bmy89960212@gamil.com>
2018-11-08 03:37:06 +0900

    도서등록 - 이미지 업로드 & php 연동 구현 중

---
+++ app/src/main/java/com/example/ggrr1/bookyou/BookRegister_EndPoints.java
@@ -0,0 +1,7 @@
+package com.example.ggrr1.bookyou;
+
+public class BookRegister_EndPoints {
+        // 서버 네트워크 연결
+        private static final String ROOT_URL = "";
+        public static final String UPLOAD_URL = ROOT_URL + "uploadpic";
+}
