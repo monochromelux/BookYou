5e4965635bd881c5d851c2b658712eae737bba11
monochromelux <ggrr12345@naver.com>
2018-11-08 03:12:25 +0900

    도서목록<->도서등록 페이지 연결

--- app/src/main/java/com/example/ggrr1/bookyou/BookList.java
+++ app/src/main/java/com/example/ggrr1/bookyou/BookList.java
@@ -1,16 +1,47 @@
 package com.example.ggrr1.bookyou;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ListView;
 import android.widget.Toast;
 
 public class BookList extends AppCompatActivity {
+
+    Button register;
+    ListView booklist;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_book_list);
+
+        booklist = (ListView) findViewById(R.id.listview);
+        register = (Button) findViewById(R.id.register);
+
+        register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), BookRegister.class);
+                startActivity(intent);
+            }
+        });
     }
+//
+//    private void dataSetting(){
+//
+//        MyAdapter mMyAdapter = new MyAdapter();
+//
+//
+//        for (int i=0; i<10; i++) {
+//            mMyAdapter.addItem(ContextCompat.getDrawable(getApplicationContext(), R.drawable.icon), "name_" + i, "contents_" + i);
+//        }
+//
+//        /* 리스트뷰에 어댑터 등록 */
+//        booklist.setAdapter(mMyAdapter);
+//    }
 
     private final long FINISH_INTERVAL_TIME = 2000;
     private long backPressedTime = 0;
---
+++ app/src/main/java/com/example/ggrr1/bookyou/ItemData.java
@@ -0,0 +1,8 @@
+package com.example.ggrr1.bookyou;
+
+public class ItemData {
+    public String bookName;
+    public String author;
+    public String price;
+    public String salePrice;
+}
