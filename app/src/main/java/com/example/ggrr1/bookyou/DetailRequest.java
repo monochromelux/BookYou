a95bfa176df771264a723b1eca2b83a5db7a76a9
minyeong <bmy89960212@gamil.com>
2018-12-04 23:05:23 +0900

    등록 수정 삭제 마무리

---
+++ app/src/main/java/com/example/ggrr1/bookyou/DetailRequest.java
@@ -0,0 +1,23 @@
+package com.example.ggrr1.bookyou;
+
+import com.android.volley.Response;
+import com.android.volley.toolbox.StringRequest;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class DetailRequest extends StringRequest {
+    final static private String URL = "http://211.213.95.132/mybookstore/book_detail.php";
+    private Map<String, String> parameters;
+
+    public DetailRequest(String book_id, Response.Listener<String> listener){
+        super(Method.POST, URL, listener, null);
+        parameters = new HashMap<>();
+        parameters.put("book_id",book_id);
+    }
+
+    @Override
+    public Map<String, String> getParams(){
+        return parameters;
+    }
+}
