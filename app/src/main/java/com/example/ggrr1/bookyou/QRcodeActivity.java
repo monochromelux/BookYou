f44e0c6c99c283fa5904a467c937875b6d4e5f3d
minyeong <bmy89960212@gamil.com>
2018-12-14 09:02:49 +0900

    책 보고 가YOU 최종본

--- app/src/main/java/com/example/ggrr1/bookyou/QRcodeActivity.java
+++ app/src/main/java/com/example/ggrr1/bookyou/QRcodeActivity.java
@@ -1,7 +1,46 @@
 package com.example.ggrr1.bookyou;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.ImageView;
+
+import com.google.zxing.BarcodeFormat;
+import com.google.zxing.MultiFormatWriter;
+import com.google.zxing.WriterException;
+import com.google.zxing.common.BitMatrix;
+import com.journeyapps.barcodescanner.BarcodeEncoder;
 
 public class QRcodeActivity extends AppCompatActivity {
 
+    int user_id;
+    ImageView imageView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_qrcode);
+
+        Intent intent = getIntent();
+        user_id = intent.getIntExtra("user_id", 1);
+
+        imageView = (ImageView) findViewById(R.id.imageView);
+
+        String textQrcode = "http://211.213.95.132/mobile_web/index.php";
+        textQrcode = textQrcode + "?user_id=" + user_id;
+
+        MultiFormatWriter multiFormatWriter = new MultiFormatWriter();
+        try {
+
+            BitMatrix bitMatrix = multiFormatWriter.encode(textQrcode, BarcodeFormat.QR_CODE, 200, 200);
+            BarcodeEncoder barcodeEncoder = new BarcodeEncoder();
+            Bitmap bitmap = barcodeEncoder.createBitmap(bitMatrix);
+            imageView.setImageBitmap(bitmap);
+
+        } catch (WriterException e) {
+            e.printStackTrace();
+        }
+
+    }
 }
